496a9517bb14498edf8c3af57bb5a5108900f8e1
mhabu <masakytkonitiwa@gmail.com>
2025-04-21 17:11:23 +0900

    Add editable created_at and 0.5ml/1ml anesthesia radio options

--- patient/templatetags/custom_filters.py
+++ patient/templatetags/custom_filters.py
@@ -1,10 +1,16 @@
 from django import template
+import datetime
 
 register = template.Library()
 
 @register.filter
 def japanese_weekday(value):
     weekdays = ['月', '火', '水', '木', '金', '土', '日']
+    
+    if isinstance(value, datetime.datetime):
+        value = value.date()  # datetime → date に変換
+    
     if hasattr(value, 'weekday'):
         return weekdays[value.weekday()]
+    
     return ''
