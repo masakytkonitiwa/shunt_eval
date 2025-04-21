0097f4c5dc3e95b750b9c59c5780ba3c502c5a73
mhabu <masakytkonitiwa@gmail.com>
2025-04-21 17:30:57 +0900

    Fix: use localtime in japanese_weekday filter to show correct weekday

--- patient/templatetags/custom_filters.py
+++ patient/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.timezone import localtime
 import datetime
 
 register = template.Library()
@@ -8,7 +9,7 @@ def japanese_weekday(value):
     weekdays = ['月', '火', '水', '木', '金', '土', '日']
     
     if isinstance(value, datetime.datetime):
-        value = value.date()  # datetime → date に変換
+        value = localtime(value).date()  # ← ここでローカル時刻に変換！
     
     if hasattr(value, 'weekday'):
         return weekdays[value.weekday()]
